6eb36a80d450482e14e2ce230f3ce13d87717f8c
Mike <Mike>
2022-07-01 09:08:47 +0700

    update linting

--- client/src/lib/socket.io/store.ts
+++ client/src/lib/socket.io/store.ts
@@ -25,7 +25,7 @@ export const state = {
   set,
   update,
 
-  setConnected(connected) {
+  setConnected(connected: boolean) {
     if (connected) {
       update((data) => ({
         ...data,
--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
-const { Server } = require("socket.io")
+import 'dotenv/config'
+import { Server } from 'socket.io'
 
-const io = new Server(3000, {
+const NODE_PORT = Number(process.env.NODE_PORT) || 3000
+
+const io = new Server(NODE_PORT, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 })
+
+console.log(`Starting on ${NODE_PORT}`)
 
 // ไปเอา random มาจาก blockchain
 // Reroll ได้
 
-let correctNumber = ~~(Math.random(0,1) * 100)
-let listResult = []
+const correctNumber = ~~(Math.random() * 100)
+const listResult = []
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`)
-  console.log("correctNumber ",correctNumber)
+  console.log('correctNumber ', correctNumber)
 
   // นับ Client
   // console.log(io.engine.clientsCount)
@@ -24,28 +29,26 @@ io.on("connection", (socket) => {
   if (io.engine.clientsCount <= 2) {
     socket.onAny((event, _number) => {
       if (event === 'play') {
-        let number = parseInt(_number, 10)
+        const number = parseInt(_number, 10)
         if (correctNumber === number) {
-          listResult.push({no:number,result:"Correct"});
+          listResult.push({ no: number, result: 'Correct' })
         } else if (correctNumber > number) {
-          listResult.push({no:number,result:"Too low"});
+          listResult.push({ no: number, result: 'Too low' })
         } else if (correctNumber < number) {
-          listResult.push({no:number,result:"Too High"});
+          listResult.push({ no: number, result: 'Too High' })
         }
-        io.emit("result",listResult)
+        io.emit('result', listResult)
       }
       // socket.broadcast.emit("receive_message", ...args)
       // ส่งหาทุกคนยกเว้น connected client ที่เรียกมา
       // socket.broadcast.emit("receive_message", "messages")
-
     })
   } else {
-    console.log("exceed player limit")
+    console.log('exceed player limit')
   }
 
   // ส่งหาเฉพาะห้อง !!!
   // socket.on("send_message", (data) => {
   //   socket.to(data.room).emit("receive_message", data);
   // });
-  
 })
